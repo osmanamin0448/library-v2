27f36468048ae5ace10da06529420e2ccfe61b48
osmanamin0448 <osmanamin0448@gmail.com>
2026-09-17 16:18:15 +0000

    Add "displayBook" function to display book on shelve Create "bookCard" element to gather book info Create variables "bookTitle,bookAuthor, bookPages, bookRead" to take book info from "myLibrary" Append bookTitle, bookAuthor, bookPage, bookRead to bookCard Append bookCard to shelve

--- script.js
+++ script.js
@@ -10,10 +10,48 @@ function Book(title,author, pages, read){
   this.read = read;
 }
 
+/* ===== Add Book to Library===== */
 function addBookToLibrary(title, author, pages, read){
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
-  console.log(myLibrary)
 }
+addBookToLibrary("me", 'him',299,"read");
+addBookToLibrary('you', "now",299,"not read")
+addBookToLibrary("me", 'him',299,"read");
+addBookToLibrary('you', "now",299,"not read")
+addBookToLibrary("me", 'him',299,"read");
+addBookToLibrary('you', "now",299,"not read")
+
+/* ===== Display Book ===== */
+function displayBook(){
+  let shelve = document.querySelector('.shelve')
+
+  for(let book of myLibrary){
+    const bookCard = document.createElement("div")
+    bookCard.classList.add("book-card")
+
+    const bookTitle = document.createElement("h1");
+
+    const bookAuthor = document.createElement("h3");
+    bookAuthor.classList.add("book-author")
+
+    const bookPages = document.createElement("p");
+    bookPages.classList.add(".book-pages");
+
+    const bookRead = document.createElement("input");
+    bookRead.type = "checkbox";
+    bookRead.classList.add("book-read");
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+    bookRead.textContent = book.read;
+
+    bookCard.append(bookTitle, bookAuthor, bookPages,bookRead)
+    shelve.append(bookCard)
+  }
+}
+
+displayBook();
 
 
