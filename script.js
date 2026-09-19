6dc6b7ff506fda4350ac1120a7059dcca7251dcb
osmanamin0448 <osmanamin0448@gmail.com>
2026-09-19 14:44:13 +0000

    Add "dialog" element in the index.html file to take info about book; Add "preventDefault" to prevent automatic submitting of form;

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ function Book(title,author, pages, read){
   this.read = read;
 }
 
-/* ===== Add Book to Library===== */
+/* ===== Add Book to Library ===== */
 function addBookToLibrary(title, author, pages, read){
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
-addBookToLibrary("me", 'him',299,"read");
-addBookToLibrary('you', "now",299,"not read")
-
 
 /* ===== Display Book ===== */
 function displayBook(){
   let shelve = document.querySelector('.shelve')
+  
+  // Clear the shelf before displaying books to prevent duplication
+  shelve.innerHTML = "";  
 
   for(let book of myLibrary){
     const bookCard = document.createElement("div")
@@ -33,7 +33,7 @@ function displayBook(){
     bookAuthor.classList.add("book-author")
 
     const bookPages = document.createElement("p");
-    bookPages.classList.add(".book-pages");
+    bookPages.classList.add("book-pages");
 
     const bookRead = document.createElement("p");
     bookRead.classList.add("book-read");
@@ -48,6 +48,37 @@ function displayBook(){
   }
 }
 
-displayBook();
+const addBook = document.querySelector(".add-book");
+const dialog = document.querySelector("dialog");
+
+/* ===== Open a Form Window to Enter book Info =====*/
+addBook.addEventListener("click", () => {
+  dialog.showModal();
+})
+
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value
+  const read = document.querySelector("#read").value
+
+  addBookToLibrary(title, author, pages, read);
+
+  displayBook();
+
+  dialog.close();
+  form.reset();
+  
+})
+
+/* ===== Close the Form Window =====*/
+const cancelBtn = document.querySelector(".cancel-btn");
+cancelBtn.addEventListener("click", () => {
+  dialog.close();
+})
 
 
